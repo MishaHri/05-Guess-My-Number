3481f6f43a598dec929bf497d215b1265c352980
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2023-04-05 18:49:37 +0100

    backgrround color when the game is win

--- starter/script.js
+++ starter/script.js
@@ -22,24 +22,42 @@ const secretNumber = Math.trunc((Math.random()*20)+1);
 let score = 20;
 document.querySelector('.number').textContent = secretNumber ;
 
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-    if (!guess) {
+
+
+    if (!guess ){
+      //when there is no number
       document.querySelector('.message').textContent = 'No value input';
-    }else if (guess === secretNumber) {
+      //when player wins
+       }else if (guess === secretNumber) {
        document.querySelector('.message').textContent = '🎉 Correct Number!';
+       document.querySelector('body').style.backgroundColor = '#60b347';
+       document.querySelector('.number').style.width = '30rem';
+
       } else if (guess > secretNumber){
+        if (score > 1) {
         document.querySelector('.message').textContent = 'too high!';
         score --;
         document.querySelector('.score').textContent = score;
+       }else{
+        document.querySelector('.message').textContent = 'You lost the game!';
+        document.querySelector('.score').textContent = 0;
+      }
       } else if (guess < secretNumber){
-      document.querySelector('.message').textContent = 'too low!';
-      score--;
-      // document.querySelector('.score').textContent = score;
-      document.querySelector('.score').textContent = score;
-     }
-  });
+        if (score >1 ){
+          document.querySelector('.message').textContent = 'too low!';
+        score--;
+          document.querySelector('.score').textContent = score;
+        }else{
+          document.querySelector('.message').textContent = 'You lost the game!';
+          document.querySelector('.score').textContent = 0;
+        }
 
 
-  
+  }
+}
+  );
