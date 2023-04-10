891418939feed1fbfb29b1fee8fbf4985ffb1afa
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2023-04-10 08:51:02 +0100

    add logic for hight score and refactor code

--- starter/script.js
+++ starter/script.js
@@ -57,7 +57,29 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 
- });
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
@@ -69,6 +91,8 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
-  document.querySelector('body').style.backgroundColor = '#5c5353';
+  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+///////////////////////////////////////
